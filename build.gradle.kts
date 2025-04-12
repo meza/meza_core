19ef2ef22393b2ea9432ddb1cded360ba2f7965b
Meza <211977+meza@users.noreply.github.com>
2025-04-12 15:11:36 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import gg.meza.stonecraft.mod
+
+plugins {
+    id("gg.meza.stonecraft")
+    id("me.modmuss50.mod-publish-plugin")
+}
+
+modSettings {
+    clientOptions {
+        fov = 90
+        guiScale = 3
+        narrator = false
+        darkBackground = true
+        musicVolume = 0.0
+    }
+}
+
+
+// Example of overriding publishing settings
+publishMods {
+    modrinth {
+        if (mod.isFabric) requires("fabric-api")
+    }
+
+    curseforge {
+        clientRequired = true // Set as needed
+        serverRequired = false // Set as needed
+        if (mod.isFabric) requires("fabric-api")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.minecraftforge.net")
+        maven("https://maven.neoforged.net/releases/")
+    }
+}
+plugins {
+    id("gg.meza.stonecraft") version "1.1.0"
+    id("dev.kikugie.stonecutter") version "0.5.2"
+}
+
+stonecutter {
+    centralScript = "build.gradle.kts"
+    kotlinController = true
+    shared {
+        fun mc(version: String, vararg loaders: String) {
+            // Make the relevant version directories named "1.20.2-fabric", "1.20.2-forge", etc.
+            for (it in loaders) vers("$version-$it", version)
+        }
+
+        mc("1.21.4", "fabric", "forge", "neoforge")
+
+        vcsVersion = "1.21.4-fabric"
+    }
+    create(rootProject)
+}
+
+rootProject.name = "YourModName"
