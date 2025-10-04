5aa3b1c65d6ff5ac9ae52ce002e1e49dada3fb4a
meza <211977+meza@users.noreply.github.com>
2025-10-04 19:14:19 +0100

    fix: disabled reminders as they weren't converting

--- src/main/java/gg/meza/supporters/SupportersCore.java
+++ src/main/java/gg/meza/supporters/SupportersCore.java
@@ -38,6 +38,7 @@ public class SupportersCore {
 
     private static final SupporterLoader LOADER = new SupporterLoader();
     private static final URI SPONSOR_URL = URI.create("https://ko-fi.com/meza");
+    private static final boolean remindersEnabled = false;
 
 
     /*? if fabric {*/
@@ -45,7 +46,9 @@ public class SupportersCore {
     public void onInitializeClient() {
         LOGGER.debug("Supporters Initializing");
         LOADER.preload();
-        FabricReminders.initialize();
+        if (remindersEnabled) {
+            FabricReminders.initialize();
+        }
     }
     /*?}*/
 
@@ -64,7 +67,9 @@ public class SupportersCore {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             LOADER.preload();
-            NeoForge.EVENT_BUS.register(NeoforgeReminders.class);
+            if (remindersEnabled) {
+                NeoForge.EVENT_BUS.register(NeoforgeReminders.class);
+            }
         }
     }
     *//*?}*/
