ab833dfcfd418efb8cb3d91f8a5c127c8fd6345e
meza <211977+meza@users.noreply.github.com>
2025-12-09 20:05:34 +0000

    ci: fixing the build

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,4 +5,4 @@ VERSION=$1
 echo "Replacing version with ${VERSION}"
 sed -e "s/0.0-SNAPSHOT/${VERSION}/" -i gradle.properties
 
-./gradlew chiseledBuildAndCollect chiseledMavenPublish --stacktrace
+./gradlew buildAndCollect publish --stacktrace
